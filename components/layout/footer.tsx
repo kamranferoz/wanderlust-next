d8af50eca6a3591e35d04b3904ca6c9d100e0f38
Kamran Feroz <kamranferoz@yahoo.com>
2023-12-28 15:12:37 +0500

    Update footer.tsx

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -2,8 +2,6 @@ import React from "react";
 import { Box, Container } from "@mui/material";
 import Typography from "@mui/material/Typography";
 import IconButton from "@mui/material/IconButton";
-import TwitterIcon from "@mui/icons-material/Twitter";
-import GithubIcon from "@mui/icons-material/GitHub";
 
 const Footer = () => (
   <Box
