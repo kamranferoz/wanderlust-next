1cfe54a95adb640238edf924d9b8fff29c6d3d87
Kamran Feroz <kamranferoz@yahoo.com>
2023-12-28 15:13:30 +0500

    Update index.ts

--- components/layout/index.ts
+++ components/layout/index.ts
@@ -1,4 +1,3 @@
-export { default as Footer } from "./footer";
 export { default as Layout } from "./layout";
 export { default as Logo } from "./logo";
 export { default as NavBar } from "./nav-bar";
